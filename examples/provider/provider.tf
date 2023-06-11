531b8a0eab055648b5c8a1bfbb4a123bc970c7d0
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2023-06-11 16:53:01 -0400

    Add provider example

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,24 @@
-provider "scaffolding" {
-  # example configuration here
+terraform {
+  required_providers {
+    lazy = {
+      source = "chrismilson/lazy"
+    }
+  }
+}
+
+variable "image_digest" {
+  type    = string
+  default = null
+}
+
+resource "lazy_string" "image_digest" {
+  explicitly = var.image_digest
+}
+
+resource "kubernetes_pod" "example" {
+  spec {
+    container {
+      image = "alpine@${lazy_string.image_digest}"
+    }
+  }
 }
