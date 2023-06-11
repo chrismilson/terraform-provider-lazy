5f1953d68e22c788dbd332cfde046499bdeecf1a
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2023-06-11 18:27:51 -0400

    Make provider example self-contained

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -12,13 +12,10 @@ variable "image_digest" {
 }
 
 resource "lazy_string" "image_digest" {
+  initially  = "sha256:200d1839f0645e5005181b6e6ebd46a040826dec2d6af9320b0f597ec9d27242"
   explicitly = var.image_digest
 }
 
-resource "kubernetes_pod" "example" {
-  spec {
-    container {
-      image = "alpine@${lazy_string.image_digest}"
-    }
-  }
+output "image" {
+  value = "alpine@${lazy_string.image_digest}"
 }
