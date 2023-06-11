d128cf1294bf65bdc90a189c4fb40eb42ce0af08
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2023-06-11 16:45:10 -0400

    Add resource tests

--- internal/provider/lazy_string_resource.go
+++ internal/provider/lazy_string_resource.go
@@ -6,127 +6,149 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var (
-  _ resource.Resource = &lazyStringResource{};
-  _ resource.ResourceWithModifyPlan = &lazyStringResource{};
+	_ resource.Resource               = &lazyStringResource{}
+	_ resource.ResourceWithModifyPlan = &lazyStringResource{}
 )
 
 type lazyStringResource struct{}
 
 func NewLazyStringResource() resource.Resource {
-  return &lazyStringResource{}
+	return &lazyStringResource{}
 }
 
 func (r *lazyStringResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-  resp.TypeName = req.ProviderTypeName + "_string"
+	resp.TypeName = req.ProviderTypeName + "_string"
 }
 
 func (r *lazyStringResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
-  resp.Schema = schema.Schema{
-    Attributes: map[string]schema.Attribute{
-      "id": schema.StringAttribute{
-        Computed: true,
-      },
-      "last_updated": schema.StringAttribute{
-        Computed: true,
-      },
-      "result": schema.StringAttribute{
-        Computed: true,
-      },
-      "initially": schema.StringAttribute{
-        Required: true,
-      },
-      "explicitly": schema.StringAttribute{
-        Optional: true,
-      },
-    },
-  }
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Description: `This is set to a random value at create time.`,
+				Computed:    true,
+			},
+			"last_updated": schema.StringAttribute{
+				Description: `This is updated to the current time whenever ` + "`result`" + ` is changing.`,
+				Computed:    true,
+			},
+			"result": schema.StringAttribute{
+				Description: `This is the canonical value for the resource. Depends on whether a value is (or has been in the past) supplied explicitly, or initially.`,
+				Computed:    true,
+			},
+			"initially": schema.StringAttribute{
+				Description: `This will determine the value of the ` + "`result`" + ` on create if no value is supplied explicitly. In general this should be a static value to avoid unnecessary terraform changes.`,
+				Computed:    true,
+				Optional:    true,
+			},
+			"explicitly": schema.StringAttribute{
+				Description: `When set, this will determine the value of ` + "`result`" + `. If unset, it will be maintained at its previous value to avoid changes in the plan.`,
+				Computed:    true,
+				Optional:    true,
+			},
+		},
+	}
 }
 
 type lazyStringResourceModel struct {
-  ID          types.String `tfsdk:"id"`
-  LastUpdated types.String `tfsdk:"last_updated"`
-  Result      types.String `tfsdk:"result"`
-  Initially   types.String `tfsdk:"initially"`
-  Explicitly  types.String `tfsdk:"explicitly"`
+	ID          types.String `tfsdk:"id"`
+	LastUpdated types.String `tfsdk:"last_updated"`
+	Result      types.String `tfsdk:"result"`
+	Initially   types.String `tfsdk:"initially"`
+	Explicitly  types.String `tfsdk:"explicitly"`
 }
 
 func (r *lazyStringResource) ModifyPlan(ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
-  var plan lazyStringResourceModel
-  var state_id types.String
-  var state_result types.String
-  var state_last_updated types.String
-  resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
-  resp.Diagnostics.Append(req.State.GetAttribute(ctx, path.Root("id"), &state_id)...)
-  resp.Diagnostics.Append(req.State.GetAttribute(ctx, path.Root("result"), &state_result)...)
-  resp.Diagnostics.Append(req.State.GetAttribute(ctx, path.Root("last_updated"), &state_last_updated)...)
+	var plan lazyStringResourceModel
+	var state lazyStringResourceModel
 
-  if resp.Diagnostics.HasError() {
-    return
-  }
+	if req.Plan.Raw.IsNull() {
+		// About to delete
+		return
+	}
 
-  if state_id != types.StringNull() {
-    plan.ID = state_id
-  }
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if req.State.Raw.IsNull() {
+		// About to create
+		state = lazyStringResourceModel{}
+	} else {
+		resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	}
 
-  if plan.Explicitly != types.StringNull() {
-    plan.Result = plan.Explicitly
-  } else if state_result != types.StringNull() {
-    plan.Result = state_result
-    plan.LastUpdated = state_last_updated
-  } else {
-    plan.Result = plan.Initially
-  }
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
-  resp.Diagnostics.Append(resp.Plan.Set(ctx, plan)...)
-  if resp.Diagnostics.HasError() {
-    return
-  }
+	if state.ID != types.StringNull() {
+		// ID should never change
+		plan.ID = state.ID
+	}
+
+	// Calculate "initially"
+	if plan.Initially == types.StringNull() || plan.Initially == types.StringUnknown() {
+		// When omitted, there should be no changes in the plan
+		plan.Initially = state.Initially
+	}
+
+	// Calculate "explicitly"
+	if plan.Explicitly == types.StringNull() || plan.Explicitly == types.StringUnknown() {
+		// When omitted, there should be no changes in the plan
+		plan.Explicitly = state.Explicitly
+	}
+
+	// Calculate "result"
+	if plan.Explicitly != types.StringNull() {
+		plan.Result = plan.Explicitly
+	} else {
+		plan.Result = plan.Initially
+	}
+
+	resp.Diagnostics.Append(resp.Plan.Set(ctx, plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 }
 
 func (r *lazyStringResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-  var plan lazyStringResourceModel
-  resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
-  if resp.Diagnostics.HasError() {
-    return
-  }
+	var plan lazyStringResourceModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
-  plan.ID = types.StringValue(fmt.Sprintf("%d", rand.Int()))
-  plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
-  
-  resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
-  if resp.Diagnostics.HasError() {
-    return
-  }
+	plan.ID = types.StringValue(fmt.Sprintf("%d", rand.Int()))
+	plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *lazyStringResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan, state lazyStringResourceModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if plan.Result != state.Result {
+		plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 }
 
 func (r *lazyStringResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
 }
-
-func (r *lazyStringResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-  var plan, state lazyStringResourceModel
-  resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
-  resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
-  if resp.Diagnostics.HasError() {
-    return
-  }
-
-  if plan.Result != state.Result {
-    plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
-  }
-  
-  resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
-  if resp.Diagnostics.HasError() {
-    return
-  }
-}
-
 func (r *lazyStringResource) Delete(_ context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
 }
-
---
+++ internal/provider/lazy_string_resource_test.go
@@ -0,0 +1,140 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccLazyStringResource_basic(t *testing.T) {
+	dsn := "lazy_string.test"
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+        resource "lazy_string" "test" {
+          initially  = "initial_value"
+          explicitly = "explicit_value"
+        }`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dsn, "id"),
+					resource.TestCheckResourceAttrSet(dsn, "last_updated"),
+					resource.TestCheckResourceAttr(dsn, "initially", "initial_value"),
+					resource.TestCheckResourceAttr(dsn, "explicitly", "explicit_value"),
+					resource.TestCheckResourceAttr(dsn, "result", "explicit_value"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccLazyStringResource_InitialValue_basic(t *testing.T) {
+	dsn := "lazy_string.test"
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+        resource "lazy_string" "test" {
+          initially  = "initial_value"
+        }`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dsn, "result", "initial_value"),
+				),
+			},
+			{
+				Config: `
+        resource "lazy_string" "test" {
+          initially  = null
+        }`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dsn, "result", "initial_value"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccLazyStringResource_ExplicitValue_SetOnCreate(t *testing.T) {
+	dsn := "lazy_string.test"
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+        resource "lazy_string" "test" {
+          initially  = "initial_value"
+          explicitly = "explicit_value"
+        }`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dsn, "result", "explicit_value"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccLazyStringResource_ExplicitValue_SetOnUpdate(t *testing.T) {
+	dsn := "lazy_string.test"
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+        resource "lazy_string" "test" {
+          initially  = "initial_value"
+          explicitly = null
+        }`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dsn, "result", "initial_value"),
+				),
+			},
+			{
+				Config: `
+        resource "lazy_string" "test" {
+          initially  = "initial_value"
+          explicitly = "explicit_value"
+        }`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dsn, "result", "explicit_value"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccLazyStringResource_ExplicitValue_Unset_NoChanges(t *testing.T) {
+	dsn := "lazy_string.test"
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: `
+        resource "lazy_string" "test" {
+          initially  = "initial_value"
+          explicitly = "explicit_value"
+        }`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dsn, "result", "explicit_value"),
+				),
+			},
+			{
+				Config: `
+        resource "lazy_string" "test" {
+          initially  = "initial_value"
+          explicitly = null
+        }`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dsn, "result", "explicit_value"),
+					resource.TestCheckResourceAttr(dsn, "explicitly", "explicit_value"),
+				),
+			},
+		},
+	})
+}
