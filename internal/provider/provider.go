486173c1a8796203ed81e9dea02c98620ba99e83
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2023-06-13 08:08:12 -0400

    Improve provider docs

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -27,7 +27,20 @@ func (p *LazyProvider) Metadata(ctx context.Context, req provider.MetadataReques
 }
 
 func (p *LazyProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
-	resp.Schema = schema.Schema{}
+	resp.Schema = schema.Schema{
+    Description: `
+    Maintaining a Terraform configuration so that it can encode several different deployment contexts is difficult.
+    Some contexts may be aware of values that others are not: If you run Terraform in CI/CD pipelines and one pipeline
+    does some preliminary calculation to set a variable, other pipelines or attempts to run the Terraform locally may
+    be difficult to implement, just because they need to work out a meaningful value to set.
+
+    The ` + "`lazy`" + ` provider is an attempt at a solution to this issue; it provides a resource that will track a 
+    value and only update its result if a new value is explicitly given. In the hypothetical situation mentioned
+    above, one pipeline might always set a value for a specific variable, while some may always leave it null. instead
+    of Terraform requiring the value to be set, the previous value will automatically be used, and there will be no
+    changes to resources that depend on that variable.
+    `,
+  }
 }
 
 func (p *LazyProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
